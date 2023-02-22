12f05e891557c3be4c6dede1a0492f10f1e86a91
Ale Machado <76435404+alemachado24@users.noreply.github.com>
2023-02-21 18:41:22 -0600

    Update soccer.py

--- soccer.py
+++ soccer.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Soccer", page_icon="⚽️",layout="wide",)
 st.sidebar.header("Soccer Forecast ⚽️")
 # st.markdown("Soccer Forecast ⚽️")
 
-@st.cache_data
+@st.cache
 def soccer_logo():
     '''
     Function to pull Soccer Picture from FiveThirtyEight.
